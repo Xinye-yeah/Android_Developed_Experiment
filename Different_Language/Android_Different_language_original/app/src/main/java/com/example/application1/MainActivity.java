d6c381397581e4b2e317b25cf69c632cb0d0b7eb
Xinye <1587181206@qq.com>
2019-11-07 14:28:19 +0800

    DifferentLanguages

---
+++ Different_Language/Android_Different_language_original/app/src/main/java/com/example/application1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text_view_language);
+        String tarString = getString(R.string.language);
+        textView.setText(tarString);
+    }
+}
